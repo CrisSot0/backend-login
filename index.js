0be6c86cfec48067b038e35ac6e53c7ed37339ca
CrisSot0 <612110440@utsalamanca.edu.mx>
2023-01-13 15:01:42 -0600

    funciona el registro

--- index.js
+++ index.js
@@ -12,10 +12,20 @@ app.use(bodyparser.urlencoded({
 app.use(bodyparser.json())
 
 //Conexion a BD
+const url = `mongodb+srv://aidcran:12345678cris@cluster0.yhveoch.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
+mongoose.connect(url,{
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+}).then(() => console.log('Conectado a la Base de Datos'))
+  .catch((error) => console.log('Error de Conexion: ' + error))
 
 //Importar las rutas
+const authRoutes = require('./routes/auth')
 
 // Ruta para el middleware
+app.use('/api/user', authRoutes)
+
+
 app.get('/', (req, res) => {
     res.json({
         estado: true,
